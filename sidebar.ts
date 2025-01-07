120136a2ef885d1b13beadf4caf61a3b3fe74680
Nate <89227631+0xNeit@users.noreply.github.com>
2025-01-07 09:17:22 +0100

    Enhance sidebar navigation and update documentation links
    - Added new sections for 'Components', 'Hooks', 'Styling', and 'Misc' in sidebar.ts with relevant documentation links.
    - Updated .vscode/settings.json to include the word "Bardock" for spell checking.
    - Removed outdated guide references in getting-started.mdx for a cleaner presentation.

--- sidebar.ts
+++ sidebar.ts
@@ -12,9 +12,42 @@ export const sidebar: Sidebar = {
       ],
     },
     {
+      text: 'Components',
+      items: [
+        { text: 'ConnectButton', link: '/docs/components/connect-button' },
+        { text: 'ConnectModal', link: '/docs/components/connect-modal' },
+        { text: 'WalletProvider', link: '/docs/components/wallet-provider' },
+      ]
+    },
+    {
+      text: 'Hooks',
+      items: [
+        { text: 'useAccountBalance', link: '/docs/hooks/use-account-balance'},
+        { text: 'useProvider', link: '/docs/hooks/use-provider'},
+        { text: 'useWallet', link: '/docs/hooks/use-wallet'},
+      ]
+    },
+    {
+      text: 'Styling',
+      items: [
+        { text: 'Basic', link: '/docs/styling/basic'},
+        { text: 'Customize CSS & Theme', link: '/docs/styling/customize'},
+        { text: 'Dark Mode', link: '/docs/styling/darkmode'},
+      ]
+    },
+    {
       text: 'Guides',
       items: [
+        { text: 'Configure Chain', link: '/docs/guides/configure-chain'},
+        { text: 'Connect Dapp', link: '/docs/guides/connect-dapp'},
+        { text: 'Customize Wallet List', link: '/docs/guides/customize-wallet-list'},
         { text: 'Use Hooks Only', link: '/docs/guides/use-hooks-only' },
+      ],
+    },
+    {
+      text: 'Misc',
+      items: [
+        { text: 'Types', link: '/docs/types' },
       ],
     },
   ],
